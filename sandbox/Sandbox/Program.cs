46e1265a7904457573da81feec49ed71e7d52295
M-Raquel <mil22025@byui.edu>
2025-04-23 09:46:13 -0600

    Seeing the different variables

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! I'm trying to see if this will push.");
+        int x = 3;
+        string s = "goodbye";
+        float f = 3.14F;
+        double d = 5.21;
+        long n = 25;
+        /* First way to do comments, multi-line comment
+        */
+        //second way to do comments. Single line comment
+        Console.WriteLine($"Hello, {s} World! I'm trying to see if this will push. {f} {d} {x}");
     }
 }
