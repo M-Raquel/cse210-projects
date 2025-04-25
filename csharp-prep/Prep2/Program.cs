1ecec2a4b378da102414b0a083e6b89e4cba40cd
M-Raquel <mil22025@byui.edu>
2025-04-25 16:12:51 -0600

    Working with conditions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your current grade percentage? ");
+        string userGrade = Console.ReadLine();
+
+        int x = int.Parse(userGrade);
+        string letterGrade = "";
+
+        if (x >= 90 )
+        {
+            letterGrade = "A";
+        }
+        else if (x >= 80 )
+        { 
+            letterGrade = "B";
+        }
+        else if (x >= 70 )
+        {
+            letterGrade = "C";
+        }
+        else if (x >= 60 )
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.Write($"Your grade is {letterGrade}. ");
+
+        if ( x >= 70 )
+        {
+            Console.WriteLine("Congragulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("We're sorry, you did not pass the course. Please contact you professor.");
+        }
     }
 }
