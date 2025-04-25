63dd5f2ab71632382aa826c61dee83e203dd1912
M-Raquel <mil22025@byui.edu>
2025-04-25 15:33:21 -0600

    Changed the Hello-World message to a Write and Read response

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,14 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        int x = 3;
-        string s = "goodbye";
-        float f = 3.14F;
-        double d = 5.21;
-        long n = 25;
+        // This is a program to compute the area of a circle
+
+        //Get radius from the user
+        Console.Write("Please enter the radius: ");
+        string text = Console.ReadLine()
+        double radius = double.Parse(text);
+
+        //Compute area of the circle.
+        double area = Math.PI * radius * radius;
+        
+        //Display the area for the user to see.
+        Console.WriteLine($"Area of the circle: {area}");
+
         /* First way to do comments, multi-line comment
         */
         //second way to do comments. Single line comment
-        Console.WriteLine($"Hello, {s} World! I'm trying to see if this will push. {f} {d} {x}");
+        Console.WriteLine($"Hello, {s} World! I'm trying to see if this will push. {f} {n} {x}");
     }
 }
