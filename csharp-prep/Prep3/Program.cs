8d440d57eab701a9c8acf682a8b75f35f41cca94
M-Raquel <mil22025@byui.edu>
2025-04-28 10:06:35 -0600

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //A variable to increment while inside the loops
+        int counter = 1;
+        // To see if the user wants to play again
+        string playAgain = "no";
+        do 
+        {
+            Random randomNumber = new Random();
+            int m = randomNumber.Next(1,50);
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            int g = int.Parse(guess);
+
+            while (g != m)
+            {
+                if (g >= m)
+                {
+                    Console.WriteLine("Lower");
+                    Console.Write("What is your guess? ");
+                    string newGuess = Console.ReadLine();
+                    g = int.Parse(newGuess);
+                    counter ++;
+
+                }
+                else 
+                {
+                    Console.WriteLine("Higher");
+                    Console.Write("What is your guess? ");
+                    string newGuess = Console.ReadLine();
+                    g = int.Parse(newGuess);
+                    counter ++;
+                }
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Your total number of guesses was {counter}");
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+
+        } while (playAgain == "yes");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,22 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is a program to compute the area of a circle
-
-        //Get radius from the user
-        Console.Write("Please enter the radius: ");
-        string text = Console.ReadLine()
-        double radius = double.Parse(text);
-
-        //Compute area of the circle.
-        double area = Math.PI * radius * radius;
-        
-        //Display the area for the user to see.
-        Console.WriteLine($"Area of the circle: {area}");
-
-        /* First way to do comments, multi-line comment
-        */
-        //second way to do comments. Single line comment
-        Console.WriteLine($"Hello, {s} World! I'm trying to see if this will push. {f} {n} {x}");
+        Console.Write("Please enter a number: ");
+        string text = Console.ReadLine();
+        float f = float.Parse(text);
+        Console.WriteLine($"Your number is {f}");
     }
-}
+}   
