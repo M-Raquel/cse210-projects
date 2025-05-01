2e5f368c12547aa7041065d3458eb2246efb3589
M-Raquel <mil22025@byui.edu>
2025-05-01 11:05:47 -0600

    Fixed calling the program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,14 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        int number = PromptUserNumber();
-        Console.WriteLine($"Your number is: {number}");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int newNumber = PromptUserNumber();
+        int squared = SquareNumber(newNumber);
+        DisplayResult(name, squared);
+    }
+    //Display a welcome message
+    static void DisplayWelcome() 
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    //Asks for and returns the user's name as a string
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
     }
 
+    //Function asking user for a number
     static int PromptUserNumber() 
     {
         Console.Write("Please enter your favorite number: ");
         int number = int.Parse(Console.ReadLine());
         return number;
+    }
+    //Accepts an interger as a parameter and returns taht number squared
+    static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+    //Calls all the functions and displays the result
+    static void DisplayResult(string name, int squared)
+    {
+        Console.Write($"{name} the square root of your number is {squared}");
     }
 }
