f3ca6ad22e0e1425997882a2c7ebfb98834b385b
M-Raquel <mil22025@byui.edu>
2025-04-30 21:53:31 -0600

    calculate the highest number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Create a list to add numbers to
+        List<int> numbersList = new List<int>();
+
+        // Create a while loop to add numbers to the list
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        int sum = 0;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbersList.Add(number);
+            sum += number;
+        }
+        Console.WriteLine($"Your sum is: {sum}");
+
+        //To calculate the average
+        int average = sum - numbersList.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        //to calculate the max number
+        int max = numbersList[0];
+        foreach ( int num in numbersList)
+        {
+            if (num > max)
+                max = num;
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
