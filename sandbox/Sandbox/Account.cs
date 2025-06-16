3c78f942de7129efcf81b26be339c1d43828846c
M-Raquel <mil22025@byui.edu>
2025-06-16 14:40:04 -0600

    Commented out programs to not cause crashes the other program activity

--- sandbox/Sandbox/Account.cs
+++ sandbox/Sandbox/Account.cs
@@ -1,32 +1,32 @@
 using System.ComponentModel;
 //Represents Encapsulation; 
-public class Account
-{
-    public List<int> _transactions = new List<int>(); // if we change this to a list...
-    private int _balance = int.MinValue;
+// public class Account
+// {
+//     public List<int> _transactions = new List<int>(); // if we change this to a list...
+//     private int _balance = int.MinValue;
 
-    public void Deposit(int amount)
-    {
-        _transactions.Add(amount);
-        _balance = int.MinValue;
+//     public void Deposit(int amount)
+//     {
+//         _transactions.Add(amount);
+//         _balance = int.MinValue;
 
-    }
+//     }
 
-    public void SetBalance(int balance)
-    {
-        _balance = balance;
-    }
-    public int GetBalance()
-    {
-        if (_balance == int.MinValue)
-        {
-            int runningBalance = 0;
-            foreach (int amount in _transactions)
-            {
-                runningBalance += amount;
-            }
-            _balance = runningBalance;
-        }
-            return runningBalance;
-    }
-}
+//     public void SetBalance(int balance)
+//     {
+//         _balance = balance;
+//     }
+//     public int GetBalance()
+//     {
+//         if (_balance == int.MinValue)
+//         {
+//             int runningBalance = 0;
+//             foreach (int amount in _transactions)
+//             {
+//                 runningBalance += amount;
+//             }
+//             _balance = runningBalance;
+//         }
+//             return runningBalance;
+//     }
+// }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,12 +1,12 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.Write("Please enter a number: ");
-        string text = Console.ReadLine();
-        float f = float.Parse(text);
-        Console.WriteLine($"Your number is {f}");
-    }
-}   
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("Please enter a number: ");
+//         string text = Console.ReadLine();
+//         float f = float.Parse(text);
+//         Console.WriteLine($"Your number is {f}");
+//     }
+// }   
