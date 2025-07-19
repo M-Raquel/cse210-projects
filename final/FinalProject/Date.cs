eb9ae06877df030e4014b15230bb61731f9471c0
M-Raquel <mil22025@byui.edu>
2025-07-18 18:43:13 -0600

    Added all of the Load from File formats for each child class as well as the Random method to create a weekly schedule

--- final/FinalProject/Date.cs
+++ final/FinalProject/Date.cs
@@ -54,7 +54,7 @@ public class Date
             DisplayDaySchedule(today);
         }
     }
-    
+
     //Displays Exercises for a specific day of user choice
     public void DisplayDaySchedule(string day)
     {
@@ -116,7 +116,6 @@ public class Date
             {
                 _weeklySchedule[day] = new List<Exercise>();
             }
-
             if (type == "CoreA")
             {
                 string name = parts[2];
@@ -137,8 +136,68 @@ public class Date
                 Core exercise = new Core(name, description, amount);
                 _weeklySchedule[day].Add(exercise);
             }
+            else if (type == "CardioA")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int time = int.Parse(parts[4]);
 
-            // Add logic for other exercise types like Cardio or Strength if needed
+                Cardio exercise = new Cardio(name, description, time);
+                _weeklySchedule[day].Add(exercise);
+            }
+            else if (type == "CardioB")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int sets = int.Parse(parts[4]);
+                int reps = int.Parse(parts[5]);
+
+                List<int> amount = new List<int> { sets, reps };
+                Cardio exercise = new Cardio(name, description, amount);
+                _weeklySchedule[day].Add(exercise);
+            }
+            else if (type == "StretchA")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int time = int.Parse(parts[4]);
+
+                Stretch exercise = new Stretch(name, description, time);
+                _weeklySchedule[day].Add(exercise);
+            }
+            else if (type == "StretchB")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int sets = int.Parse(parts[4]);
+                int reps = int.Parse(parts[5]);
+
+                List<int> amount = new List<int> { sets, reps };
+                Stretch exercise = new Stretch(name, description, amount);
+                _weeklySchedule[day].Add(exercise);
+            }
+            else if (type == "StrengthA")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int time = int.Parse(parts[4]);
+
+                Strength exercise = new Strength(name, description, time);
+                _weeklySchedule[day].Add(exercise);
+            }
+            // format = $"StrengthB| {_name}| {_description}| {_weight}| {_setAmount[0]}|{_setAmount[1]}";
+            else if (type == "StrengthB")
+            {
+                string name = parts[2];
+                string description = parts[3];
+                int weight = int.Parse(parts[4]);
+                int sets = int.Parse(parts[5]);
+                int reps = int.Parse(parts[6]);
+
+                List<int> amount = new List<int> { sets, reps, weight };
+                Strength exercise = new Strength(name, description, amount, weight);
+                _weeklySchedule[day].Add(exercise);
+            }
         }
 
         Console.WriteLine($"Weekly schedule loaded from {filename}");
@@ -149,10 +208,22 @@ public class Date
     {
         foreach (string day in _weeklySchedule.Keys)
         {
+            // Random Core
             Exercise e1 = new Core(); e1.Random();
-            // Add Flexibility and other stuff
+            // Random Cardio
+            Exercise e2 = new Cardio(); e2.Random();
+            // Random Stretch
+            Exercise e3 = new Stretch(); e3.Random();
+            // Random Strength
+            Exercise e4 = new Strength(); e4.Random();
+
 
             _weeklySchedule[day].Add(e1);
+            _weeklySchedule[day].Add(e2);
+            _weeklySchedule[day].Add(e3);
+            _weeklySchedule[day].Add(e4);
+
+
         }
     }
 
