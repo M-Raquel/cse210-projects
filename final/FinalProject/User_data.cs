5bf23369750ba86b61d29b7de17977b5f57a806f
M-Raquel <mil22025@byui.edu>
2025-07-18 19:51:10 -0600

    Added comments to User_data to make easier to read, and also changed the Flexibility Class to Stretch Class

--- final/FinalProject/User_data.cs
+++ final/FinalProject/User_data.cs
@@ -18,6 +18,8 @@ public class UserData
     }
 
     //Methods
+
+    // Displays the User Information
     public void DisplayUserInfo()
     {
         Console.WriteLine($"{_userName} -- Age: {_userAge} Weight: {_userWeight}");
@@ -25,15 +27,27 @@ public class UserData
 
     //Methods to Create Exercises
 
+    // Used to get the sets and reps for each other create method
+    public static List<int> CoreAmountInput()
+    {
+        Console.Write("Enter number of sets: ");
+        int sets = int.Parse(Console.ReadLine());
+        Console.Write("Enter number of reps: ");
+        int reps = int.Parse(Console.ReadLine());
+        return new List<int> { sets, reps };
+    }
+
+
+    // Creates a new Core Exercise
     public static Exercise CreateCoreExercise()
     {
-    Console.Write("Enter name: ");
-    string name = Console.ReadLine();
-    Console.Write("Enter description: ");
-    string description = Console.ReadLine();
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter description: ");
+        string description = Console.ReadLine();
 
-    Console.Write("Use time-based format? (yes/no): ");
-    string useTime = Console.ReadLine().ToLower();
+        Console.Write("Use time-based format? (yes/no): ");
+        string useTime = Console.ReadLine().ToLower();
 
         if (useTime.StartsWith("y"))
         {
@@ -47,10 +61,55 @@ public class UserData
         }
     }
 
-    public static Exercise CreateCardioExercise() => CreateCoreExercise(); 
+    // Creates a new Cardio Exercise
+    public static Exercise CreateCardioExercise()
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter description: ");
+        string description = Console.ReadLine();
 
-    public static Exercise CreateFlexibilityExercise() => CreateCoreExercise();
+        Console.Write("Use time-based format? (yes/no): ");
+        string useTime = Console.ReadLine().ToLower();
 
+        if (useTime.StartsWith("y"))
+        {
+            Console.Write("Enter time in minutes: ");
+            int time = int.Parse(Console.ReadLine());
+            return new Cardio(name, description, time);
+        }
+        else
+        {
+            List<int> amount = CoreAmountInput();
+            return new Cardio(name, description, amount);
+        }
+    } 
+
+    // Creates a new Stretch Exercise
+    public static Exercise CreateStretchExercise()
+    {
+        Console.Write("Enter name: ");
+        string name = Console.ReadLine();
+        Console.Write("Enter description: ");
+        string description = Console.ReadLine();
+
+        Console.Write("Use time-based format? (yes/no): ");
+        string useTime = Console.ReadLine().ToLower();
+
+        if (useTime.StartsWith("y"))
+        {
+            Console.Write("Enter time in minutes: ");
+            int time = int.Parse(Console.ReadLine());
+            return new Stretch(name, description, time);
+        }
+        else
+        {
+            List<int> amount = CoreAmountInput();
+            return new Stretch(name, description, amount);
+        }
+    }
+
+    // Creates a new Strength Exercise
     public static Exercise CreateStrengthExercise()
     {
         Console.Write("Enter name: ");
@@ -76,12 +135,4 @@ public class UserData
         }
     }
 
-    public static List<int> CoreAmountInput()
-    {
-        Console.Write("Enter number of sets: ");
-        int sets = int.Parse(Console.ReadLine());
-        Console.Write("Enter number of reps: ");
-        int reps = int.Parse(Console.ReadLine());
-        return new List<int> { sets, reps };
-    }
 }
